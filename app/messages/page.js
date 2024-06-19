df7b4076fb58afa2bd641a86dedd44779a5d7467
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-19 06:20:09 +0530

    learn about request de-duplication

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,22 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
+import { unstable_noStore } from 'next/cache';
+
+// export const revalidate = 0;
+// export const dynamic = 'force-dynamic'; // same as setting as 'no-store' in cache
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+  unstable_noStore();
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   // cache: 'no-store',
+  //   // next: {
+  //   //   revalidate: 0, // time in seconds
+  //   // },
+  // });
+  // const messages = await response.json();
+
+  const messages = await getMessages();
+  console.log({ messages });
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
